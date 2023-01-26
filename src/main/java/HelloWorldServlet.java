cbe5f7f50ac6fb4bc1055f7f1ae157c5759174e6
raynaldo-deanda <reynaldo.deanda@gmail.com>
2023-01-26 14:29:21 -0600

    update

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        writer.println("<h1>Hello World!</h1>");
     }
 }
---
+++ src/main/java/ViewCountServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ViewCountServlet", urlPatterns ="count")
+public class ViewCountServlet extends HttpServlet {
+
+    private int count = 0;
+    @Override
+    protected  void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String reset = request.getParameter("reset");
+        if(reset != null) {
+            count = 0;
+        } else {
+            count++;
+        }
+        count++;
+        out.println("<h1>Count:"+ count + "</h1>");
+    }
+}
