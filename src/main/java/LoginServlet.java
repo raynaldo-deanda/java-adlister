def905c17b3982475e41270204523ed9f4010c77
raynaldo-deanda <reynaldo.deanda@gmail.com>
2023-02-08 16:06:25 -0600

    completed exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -5,26 +5,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/login.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        if (req.getMethod().equalsIgnoreCase("post")) {
-            String username = req.getParameter("username");
-            String password = req.getParameter("password");
-            if (username.equals("admin") && password.equals("password")) {
-                resp.sendRedirect("/profile");
-            } else {
-                resp.sendRedirect("/login");
-            }
-            }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("user") != null){
+            response.sendRedirect("/profile");
+            return;
         }
-
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean validAttempt = username.equals("admin") && password.equals("password");
+
+        if (validAttempt) {
+            request.getSession().setAttribute("user", username);
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
+
 
